7a12e8e91b9092c401df80eb35deba2d2de5a292
Bikram Shrestha <bikrams@infinite.com>
2025-06-05 14:35:24 +0545

    second commit

--- test_login.py
+++ test_login.py
@@ -5,32 +5,44 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-with open('C:/Users/Acer/OneDrive/Desktop/Self-healing/self-heating-automation-ai/locators.json', 'r') as f:
+with open('C:/Users/bikrams/Desktop/Self-healing/self-healing-automation-ai/locators.json', 'r') as f:
     locators = json.load(f)
 
 driver = webdriver.Chrome()
 driver.get("http://localhost:4200")
 time.sleep(1)
 
-# === Use dynamic locators ===
-driver.find_element(By.ID, locators["usernameInput"]).send_keys("testuser")
-time.sleep(0.5)
-
-driver.find_element(By.ID, locators["passwordInput"]).send_keys("testpass")
-time.sleep(0.5)
-
-driver.find_element(By.CSS_SELECTOR, locators["loginButton"]).click()
-time.sleep(1)
-
-# === Wait for dashboard ===
+# Login
 try:
-    dashboard_element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, locators["dashboardWelcome"]))
-    )
+    driver.find_element(By.ID, locators["usernamesInput"]).send_keys("testuser")
+    time.sleep(0.5)
+    driver.find_element(By.ID, locators["passwordInput"]).send_keys("testpass")
+    time.sleep(0.5)
+    driver.find_element(By.CSS_SELECTOR, locators["loginButton"]).click()
+    time.sleep(1)
+
+   
     print("✅ Login successful. Dashboard loaded.")
 
+    add_card_button = driver.find_element(By.ID, locators["addCardButton"])
+    add_card_button.click()
+    time.sleep(1)
+    print("✅ Card added.")
+
+    cards = driver.find_elements(By.CSS_SELECTOR, locators["card1"])
+    if len(cards) > 0:
+        print(f"✅ {len(cards)} card(s) present after adding.")
+
+    delete_card_button = cards[0].find_element(By.CSS_SELECTOR, locators["delete-card-button-1"])
+    delete_card_button.click()
+    time.sleep(1)
+    print("✅ Card deleted.")
+
+    cards = driver.find_elements(By.CSS_SELECTOR, locators["card1"])
+    print(f"✅ {len(cards)} card(s) present after deleting.")
+
 except Exception as e:
-    print("❌ Login failed or dashboard not loaded:", str(e))
+    print("❌ An error occurred:", str(e))
 
 finally:
     time.sleep(2)
